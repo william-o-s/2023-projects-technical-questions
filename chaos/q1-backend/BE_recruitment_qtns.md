890c986847770756952f8555215b9d810d2de875
William Setiawan <z5388080@ad.unsw.edu.au>
2023-01-16 23:11:22 +1100

    Completed chaos q3

--- chaos/q1-backend/BE_recruitment_qtns.md
+++ chaos/q1-backend/BE_recruitment_qtns.md
@@ -14,11 +14,13 @@
         }).join().unwrap();
     ```
 
-    A:
+    A: This code does not compile due to ref_to_ref_cell borrowing my_ref_cell and thereafter being used within a separate spawned thread. This defies Rust's borrowing rules on references, which cannot be used in a multithreaded context, only single threads.
+    
+    For disclosure, I needed to first compile this code, then discover the compile error, and only then answer this question after reading the [docs](https://doc.rust-lang.org/book/ch15-05-interior-mutability.html).
 
 2.  Shortly discuss, when modelling a response to a HTTP request in Rust, would you prefer to use `Option` or `Result`?
 
-    A:
+    A: I would use a `Result` for HTTP responses. Due to the variety of error status codes (404, 500, etc.), the `Err` variant in a `Result` is more suited to handle these scenarios than the `Some` variant in an `Option`. This additionally follows HTTP responses' format of having a single `Ok` variant (status 200) instead of an ambiguous `Some` variant.
 
 3.  In `student.psv` there are some fake student datas from UNSW CSE (no doxx!). In each row, the fields from left to right are
 
@@ -34,3 +36,11 @@
 
     Write a Rust program to find the course which has the highest average student WAM. **Write your program in the cargo project q3**.
 
+    A: Completed. This was a really tough challenge, as there is still much more to learn in Rust - but breaking down the problem assisted greatly in thinking about the next step. The code could definitely be made cleaner - I could have separated much of the code into their own functions, but I was unsure of their signatures. Overall, fantastic challenge. Thanks!
+
+    References:
+        1. [PathBuf](https://doc.rust-lang.org/std/path/struct.PathBuf.html)
+        2. [csv Crate](https://docs.rs/csv/latest/csv/tutorial/index.html)
+        3. [csv ReaderBuilder](https://docs.rs/csv/latest/csv/struct.ReaderBuilder.html)
+        4. [HashMap example](https://doc.rust-lang.org/std/collections/struct.HashMap.html)
+        5. [HashMap snippets](https://doc.rust-lang.org/book/ch08-03-hash-maps.html)
--- chaos/q1-backend/q3/src/main.rs
+++ chaos/q1-backend/q3/src/main.rs
@@ -1,18 +1,74 @@
 // q3
 // In `student.psv` there are some fake student datas from UNSW CSE (no doxx!). In each row, the fields from left to right are
 //
-// - UNSW Course Code
+// - ! UNSW Course Code
 // - UNSW Student Number
 // - Name
 // - UNSW Program
 // - UNSW Plan
-// - WAM
+// - ! WAM
 // - UNSW Session
 // - Birthdate
 // - Sex
 // 
 // Write a Rust program to find the course which has the highest average student WAM.
 
+use std::env;
+use std::path::PathBuf;
+use csv::ReaderBuilder;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    // Read current directory, which is one level below student.psv directory
+    let mut cur_dir = env::current_dir().unwrap();
+    cur_dir.pop();
+    cur_dir.push("student.psv");
+    
+    // Create PSV reader for student.psv file
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b'|')
+        .from_path(PathBuf::from(cur_dir)).unwrap();
+
+    // To calculate average, we need a sum of WAM and a count of course code occurrences.
+    // 2 HashMaps will be used for this.
+    let mut map_wam_sum: HashMap<String, f64> = HashMap::new();
+    let mut map_course_occur: HashMap<String, f64> = HashMap::new();
+
+    const RECORD_COURSE_CODE_IDX: usize = 0;
+    const RECORD_WAM_IDX: usize = 5;
+
+    // Iterate over records to store required values.
+    for row in reader.records() {
+        // Took an abnormally long time to understand: this iterator produces a slice, which is then dropped
+        // on the next iteration!!! Thus, a type of &str cannot be used on course_code, and to_string() must
+        // be called to convert it.
+        let course_code = row.as_ref().unwrap().get(RECORD_COURSE_CODE_IDX).unwrap().to_string();
+        let wam = row.as_ref().unwrap().get(RECORD_WAM_IDX).unwrap().parse::<f64>().unwrap();
+
+        // course_code, since Copy is not implemented, will be moved when accessed.
+        // A clone must be used so that the next call is not broken.
+        let value = map_wam_sum.entry(course_code.clone()).or_insert(0.0);
+        *value += wam;
+        
+        let value = map_course_occur.entry(course_code).or_insert(0.0);
+        *value += 1.0;
+    }
+
+    // Ensure both HashMaps have the same length.
+    assert_eq!(map_wam_sum.len(), map_course_occur.len());
+
+    // Iterate over both HashMaps, calculating a running average.
+    let mut highest_course_code = String::from("");
+    let mut highest_average_wam = 0.0;
+    for (course_code, wam_sum) in &map_wam_sum {
+        let average_wam = *wam_sum / *map_course_occur.get(course_code).unwrap();
+
+        if average_wam > highest_average_wam {
+            highest_course_code = course_code.to_string();
+            highest_average_wam = average_wam;
+        }
+    }
+
+    println!("The course {} has the highest average WAM of {}.", highest_course_code, highest_average_wam);
 }
